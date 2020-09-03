ea18e93e5c6cca3dbb208043ddeeef35c44c8527
YongGyunKwon <ygkwon50@gmail.com>
2020-09-04 02:37:24 +0900

    change accesibillity.js

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -1,12 +1,13 @@
 export const accessibilityCollection = {
-	name: "Accessibility",
+	name: "MAJOR",
 	areas: [
 		{
+			name:"선수 과목",
 			skills: [
 				{
-					name: "The why",
+					name: "컴퓨터시스템개론",
 					description: {
-						text: "Learn what accessibility is and why it is important.",
+						text: "컴퓨터에 대한 기초 지식을 부여하는 과목으로서 하드웨어, 소프트웨어 및 Firmware의 기능을 정의하고 프로그램과 컴퓨터 상호간에 관계 및 문제의 해결을 컴퓨터로 처리하는 방법을 습득한다.",
 						links: [
 							["Google Devs - Accessibility Fundamentals", "https://developers.google.com/web/fundamentals/accessibility/"],
 							["A11ycasts with Rob Dodson", "https://www.youtube.com/playlist?list=PLNYkxOF6rcICWx0C9LVWWVqvHlYJyqw7g"],
@@ -16,45 +17,65 @@ export const accessibilityCollection = {
 							["Udacity - Web Accessibility", "https://www.udacity.com/course/web-accessibility--ud891"],
 							["Youtube - Headings, Landmarks, and Tabs", "https://www.youtube.com/watch?v=HE2R86EZPMA"],
 						]
-					}
+					},
+					skills:[
+						{
+							name:"자료구조",
+							description:{
+								text:"컴퓨터에서 처리하는 데이터 기본 개념과 이를 효율적으로 처리하기 위한 자료 구조인 Array, Stack, Queue 및 Linked List 등의 자료구조에 대하여 공부한다. 또는 이를 이용한 Tree Manipulation, 그래프 이론, 자료 정렬 및 검색과 Symbol Table 작성 방법에 대하여 학습한다."
+							},
+							skills:[
+								{
+									name:"데이터베이스시스템",
+									description:{
+										text:"이 과목에서는 데이터베이스의 기본 개념과 개체-관계(E-R) 다이어그램, 관계형 데이터 모델 및 SQL 문의 사용, 데이터베이스 설계, 데이터베이스 보안 등은 물론 DBMS 구조와 그 기능을 공부한다. 아울러 실제 응용에서 시스템 개발과 웹의 연동 방법 등을 학습한다."
+
+									}
+								},
+								{
+									name:"알고리즘",
+									description:{
+										text:"일반적으로 널리 알려진 알고리즘 및 대표적인 응용분야에 적용되는 알고리즘 기능 및 처리 과정을 배운다. 알고리즘의 난해도를 분석하고 효율적인 알고리즘을 설계하는 기법을 익힌다."
+									}
+								}
+							]
+						}
+					]	
 				},
 				{
-					name: "Screen Readers",
+					name: "이산수학",
 					description: {
-						text: "Learn about assistive technology such as screen readers that reads sections of the page aloud based on the current focus.",
+						text: "컴퓨터과학 및 디지털정보처리 분야의 수학적 기초가 되는 논리, 함수, 관계를 포함한 집합, 알고리즘, 수학적 귀납법 및 증명방법, 그래프, 트리, 부울 대수 등을 학습한다.",
 						links: [
 							["The A11Y Project - Screen Reader Myths", "https://a11yproject.com/posts/people-who-use-screen-readers-dont-use-javascript/"],
 							["The A11Y Project - NVDA Screen Reader", "https://a11yproject.com/posts/getting-started-with-nvda/"],
 							["The A11Y Project - OS X Voiceover", "https://a11yproject.com/posts/getting-started-with-voiceover/"],
 							["Webaim - Screen Reader Survey", "https://webaim.org/projects/screenreadersurvey7/"]
 						]
-					}
+					},
+					skills:[
+						{
+							name:"알고리즘",
+							description:{
+								text:"일반적으로 널리 알려진 알고리즘 및 대표적인 응용분야에 적용되는 알고리즘 기능 및 처리 과정을 배운다. 알고리즘의 난해도를 분석하고 효율적인 알고리즘을 설계하는 기법을 익힌다."
+							}
+						}
+					]
 				},
 				{
-					name: "Accessibility tree",
+					name: "응용 컴퓨터 프로그래밍",
 					description: {
-						text: "Learn about the accessibility tree and how assistive technology uses it.",
+						text: ".....",
 						links: [
 							["Google Devs - The Accessibility Tree", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/the-accessibility-tree"]
 						]
 					},
 					skills: [
+						
 						{
-							name: "ARIA",
+							name: "객체지향 프로그래밍",
 							description: {
-								text: "Learn how to use ARIA descriptions an labels to help assistive technology understanding your website.",
-								links: [
-									["Google Devs - ARIA Labels", "https://developers.google.com/web/fundamentals/accessibility/semantics-aria/aria-labels-and-relationships"],
-									["Google Devs - Hiding and updating content", "https://developers.google.com/web/fundamentals/accessibility/semantics-aria/hiding-and-updating-content"],
-									["The A11Y Project - ARIA States", "https://a11yproject.com/posts/ARIA-states/"],
-									["The A11Y Project - ARIA Properties", "https://a11yproject.com/posts/ARIA-properties/"],
-								]
-							}
-						},
-						{
-							name: "Accessible HTML",
-							description: {
-								text: "Learn how to write HTML in such as way that assistive technology better understands it.",
+								text: "객체지향 개념 및 프로그래밍 기법에 대한 이론적 학습과 실습을 통하여 객체지향 프로그램을 개발하고, 실제 응용 프로그래밍에 활용할 수 있는 능력을 함양한다.",
 								links: [
 									["Google Devs - Introduction to Semantics", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/"],
 									["MDN - Accessibility HTML", "https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML"],
@@ -65,119 +86,334 @@ export const accessibilityCollection = {
 							},
 							skills: [
 								{
-									name: "Alt text",
+									name: "객체지향 설계",
 									description: {
-										text: "Learn how to use the alt attribute to provide a useful text alternative to this image.",
+										text: "객체지향 개발 방법에 의하여 소프트웨어를 분석하고 설계하기 위한 기본 개념의 이해 및 기법들을 대하여 학습하고, 프로젝트를 통한 문제 분석 및 설계 과정을 진행한다.",
 										links: [
 											["Google Devs - Text Alternatives for Images", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/text-alternatives-for-images"],
 											["Webaim - Accessible Images", "https://webaim.org/techniques/images/"],
 											["Webaim - Alternative text", "https://webaim.org/techniques/alttext/"],
 										]
-									}
+									},
+									skills: [
+										{
+											name: "소프트웨어 공학",
+											description: {
+												text: "소프트웨어 공학에 대한 기본적인 개념의 이해와 소프트웨어 개발 과정에서 품질을 향상시키기 위해 요구되는 공학적 활동에 대하여 학습하고, 주어진 문제의 소프트웨어 개발을 위한 프로젝트를 진행한다.",
+												links: [
+													["Google Devs - Text Alternatives for Images", "https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/text-alternatives-for-images"],
+													["Webaim - Accessible Images", "https://webaim.org/techniques/images/"],
+													["Webaim - Alternative text", "https://webaim.org/techniques/alttext/"],
+												]
+											}
+										}
+									]
 								}
 							]
-						},
-						{
-							name: "Accessible CSS",
-							description: {
-								text: "Learn how to write CSS in such as way that assistive technology better understands it.",
-								links: [
-									["Webaim - CSS", "https://webaim.org/techniques/css/"]
-								]
-							}
-						},
+						}
+						
 					]
 				},
 				{
-					name: "Accessible forms",
+					name: "컴퓨터 구조",
 					description: {
-						text: "Learn how to build accessible forms that makes it usable to as many people as possible.",
+						text: "컴퓨터 관련 전공자들이 필수적으로 알고 있어야 할 컴퓨터 하드웨어의 동작 원리에 대하여 학습한다. 컴퓨터의 구성 요소인 중앙처리장치, 기억장치, 입출력장치의 동작 원리를 이해하고, 중앙처리장치가 명령어를 실행하는 과정에 대하여 자세히 배운다.",
 						links: [
 							["Webaim - Forms", "https://webaim.org/techniques/forms/"],
 							["Itnext - Form Accessibility Gudie", "https://itnext.io/form-accessibility-a-practical-guide-4062b7e2dd14"]
 						]
-					}
-				},
-				{
-					name: "UI States",
-					description: {
-						text: "Learn how to make the state of each UI element clear.",
-						links: [
-							["Google Devs - Accessible Styles", "https://developers.google.com/web/fundamentals/accessibility/accessible-styles"]
-						]
-					}
-				},
-				{
-					name: "Keyboard Accessibility",
-					description: {
-						text: "Learn how to make it easy for keyboard users to navigate your site.",
-						links: [
-							["Webaim - Keyboard", "https://webaim.org/techniques/keyboard/"],
-							["Smashing Magazine - Web with just a keyboard", "https://www.smashingmagazine.com/2018/07/web-with-just-a-keyboard/"]
-						]
 					},
-					skills: [
+					skills:[
 						{
-							name: "Focus",
+							name:"운영체제",
 							description: {
-								text: "Learn how to create a sensible tab order and how to make it easy for the users to locate the currently focused element.",
-								links: [
-									["web.dev - Control focus with tabindex", "https://web.dev/control-focus-with-tabindex/"],
-									["Google Devs - Focus", "https://developers.google.com/web/fundamentals/accessibility/focus/"],
-									["Google Devs - Dom Order Matters", "https://developers.google.com/web/fundamentals/accessibility/focus/dom-order-matters"],
-									["Google Devs - Using Tabindex", "https://developers.google.com/web/fundamentals/accessibility/focus/using-tabindex"],
-									["Google Devs - Track Focus", "https://developers.google.com/web/tools/chrome-devtools/accessibility/focus"],
-									["MDN - :focus-visible", "https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-visible"],
-									["MDN - :focus-within", "https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-within"]
-								]
+								text:"운영체제, 특히 다중 프로그래밍 시분할 시스템, 비동기 병행프로세서의 기본개념을 다룬다. 스케쥴링, 메모리 관리 정보의 고유 및 보호 등의 개념을 습득한다."
+							}
+						},
+						{
+							name:"펌웨어프로그래밍",
+							description: {
+								text:"마이크로컴퓨터 시스템의 하드웨어 구성 요소를 이해하고, 마이크로프로세서 실습 장비를 사용하여 하드웨어를 제어하기 위한 시스템 시작 코드 해석, 인터럽트 및 타이머 제어 프로그래밍, 입출력 디바이스 제어를 위한 펌웨어 프로그래밍 기술을 습득한다."
 							}
 						}
 					]
 				},
 				{
-					name: "Accessible Colors",
+					name: "창의공학설계",
 					description: {
-						text: "Learn how to select colors in such a way that users, including those with visual disabilities, can perceive the content on the page.",
-						links: [
-							["Webaim - Contrast and Color", "https://webaim.org/articles/contrast/"],
-							["The A11Y Project - Check contrast", "https://a11yproject.com/posts/check-contrast-with-mobile-device/"],
-							["The A11Y Project - What is color contrast?", "https://a11yproject.com/posts/what-is-color-contrast/"],
-							["Designing accessible color systems", "https://stripe.com/en-dk/blog/accessible-color-systems"],
-						]
+						text: "여러 가지 다양한 문제의 해결 과정을 통해서 참여 학생의 창의적 능력을 키우며, 문제의 해결에 대한 능력 향상과 함께 팀활동을 체험한다."
+								
+					},
+					skills:[
+						{
+							name:"산학프로젝트(종합설계)",
+							description:{
+								text:"산업체(가족회사)의 멘토 또는 취업 선배(멘토)와의 연계를 통해 산업체가 수행하고 있는 프로젝트를 이해하고, 산업체에서 프로젝트 수행에 따른 문제점과 이에 대한 해결 방안을 멘토와 함께 해결함으로써, 현장의 실전을 이해한다."
+							},
+							skills:[
+								{
+									name:"캡스톤디자인",
+									description:{
+										text:"컴퓨터과학의 기본 이론을 바탕으로 팀을 구성하여 주제를 선정하고, 프로젝트 진행 방법을 계획하여, 최종 결과물을 도출하기까지의 프로세스를 체계적으로 준수하면서 팀 기반의 프로젝트 수행을 경험한다."
+									},
+									skills:[
+										{
+											name:"창업파일럿프로젝트",
+											description:{
+												text:"...."
+											}
+										}
+									]
+								}
+							]
+
+						}
+					]
+				}
+			]
+		},
+		{
+			name:"비선수과목",
+			skills:[
+				{
+					name:"미래설계탐색",
+					description:{
+						text:"스트롱 직업흥미검사를 온라인으로 실시하고, 해석 워크샵을 그 결과를 알게함으로써 자신의 직업적 흥미 경향성을 알아보고, 지속적인 상담 및 지도를 통해 자기를 이해하고 개인 가치관을 확립한다."
+
+					}	
+				},
+				{
+					name:"오픈소스소프트웨어이해와실습",
+					description:{
+						text:"......."
+
+					}	
+				},
+				{
+					name:"미래설계준비",
+					description:{
+						text:"......."
+
+					}	
+				},
+				{
+					name:"소프트웨어실전영어",
+					description:{
+						text:"......."
+
+					}	
+				},
+				{
+					name:"선형대수학",
+					description:{
+						text:"행렬과 연립방정식, 행렬식, 벡터공간, 선형변환, 고유값, 특이값 분해 등의 이해를 통해 전산 응용 수학의 기초지식을 습득한다."
+
+					}	
+				},
+				{
+					name:"선형대수학",
+					description:{
+						text:"행렬과 연립방정식, 행렬식, 벡터공간, 선형변환, 고유값, 특이값 분해 등의 이해를 통해 전산 응용 수학의 기초지식을 습득한다."
+
+					}	
+				},
+				{
+					name:"미레설계구현",
+					description:{
+						text:"...."
+
+					}	
+				},
+				{
+					name:"오픈소스기초프로젝트",
+					description:{
+						text:"행렬과 연립방정식, 행렬식, 벡터공간, 선형변환, 고유값, 특이값 분해 등의 이해를 통해 전산 응용 수학의 기초지식을 습득한다."
+
+					}	
+				},
+				{
+					name:"프로그래밍언어론",
+					description:{
+						text:"이 과목은 컴퓨터 프로그래밍 언어에 적용되고 있는 기본 개념에 대해서 학습하고 최신 프로그래밍 언어가 가지고 있는 특징을 개괄한다. 이를 통하여 사용할 프로그램 언어를 선택할 수 있는 능력을 배양하고 프로그램 언어의 설계 능력을 갖추도록 한다."
+
+					}	
+				},
+				{
+					name:"시스템프로그래밍",
+					description:{
+						text:"컴퓨터 시스템의 효율적인 사용이 요구되는 하드웨어와 소프트웨어 구조의 이해, Assembly Language 이해를 통해 Assembler, Loader 등의 설계방법 도입"
+
+					}	
+				},
+				{
+					name:"창업탐색",
+					description:{
+						text:"......"
+
+					}	
+				},
+				{
+					name:"오픈소스개발프로젝트",
+					description:{
+						text:"오픈 소스 프로그래밍 언어인 자바를 이용하여 실생활에 적용 가능한 소프트웨어를 개발하고, 이러한 개발과정에서 프로그램의 역할과 프로그램 코드의 체계적인 개발에 대한 중요성을 이해한다."
+
+					}	
+				},
+				{
+					name:"확률및통계",
+					description:{
+						text:"통계적 개념과 통계적 사고방법의 도입을 통해 전자계산기 시스템 분야에서의 응용 능력 배양"
+
+					}	
+				},
+				{
+					name:"확률및통계",
+					description:{
+						text:"통계적 개념과 통계적 사고방법의 도입을 통해 전자계산기 시스템 분야에서의 응용 능력 배양"
+
+					}	
+				},
+				{
+					name:"컴퓨터그래픽스",
+					description:{
+						text:"컴퓨터 그래픽스 분야를 구성하는 렌더링, 애니메이션, 그리고 기하모델링 등의 기본 주제에 관한 이론과 실제를 다루며 대표적인 실시간 3차원 그래픽스 프로그래밍 툴을 사용하여 그래픽스 프로그래밍 기법을 익힌다."
+
 					}
 				},
 				{
-					name: "Laws & Policies",
-					description: {
-						text: "Learn about the governmental policies related to web accessibility.",
-						links: [
-							["W3C - Laws & Policies", "https://www.w3.org/WAI/policies/"],
-							["Webaim - Laws around the world", "https://webaim.org/articles/laws/world/"],
-							["Webaim - Section 508", "https://webaim.org/standards/508/checklist"],
-							["Webaim - WCAG 2 Checklist", "https://webaim.org/standards/wcag/checklist"],
-							["A11Y.dev - Lawsuits", "https://a11y.dev/470-digital-accessibility-cases-were-filed-in-q3-2018/"],
-						]
-					},
-					skills: [
-						{
-							name: "Audits",
-							description: {
-								text: "Learn how to conduct an accessibility review to improve the overall experience of using your site. Remember, good accessibility equals good UX!",
-								links: [
-									["web.dev - Accessibility audits", "https://web.dev/lighthouse-accessibility"],
-									["Google Devs - How to review", "https://developers.google.com/web/fundamentals/accessibility/how-to-review"],
-									["Webaim - Accessibility Tools", "https://webaim.org/articles/tools/"],
-									["Google Devs - Accessibility Reference", "https://developers.google.com/web/tools/chrome-devtools/accessibility/reference"],
-									["Webaim - Wave", "http://wave.webaim.org/"],
-									["Google Chrome - NoCoffee Extension", "https://chrome.google.com/webstore/detail/nocoffee/jjeeggmbnhckmgdhmgdckeigabjfbddl"],
-									["Digital A11Y - Accessibility Plugins", "https://www.digitala11y.com/accessibility-plug-ins-ie-chrome-firefox-browsers/"],
-									["Google Devs - Accessibility for teams", "https://developers.google.com/web/fundamentals/accessibility/a11y-for-teams"],
-									["WCAG 2.0 checklists", "https://www.wuhcag.com/wcag-checklist/"],
-									["How to Meet WCAG (Quick Reference)", "https://www.w3.org/WAI/WCAG21/quickref/"],
-								]
-							}
-						}
-					]
+					name:"오픈소스 웹소프트웨어",
+					description:{
+						text:"...."
+
+					}	
+				},
+				{
+					name:"컴파일러",
+					description:{
+						text:"최신 컴파일러를 중심으로 어휘분석, 파싱 및 코드생성 등 일련의 컴파일 과정을 학습한다. 특히 컴퓨터 프로그래밍 언어가 가지고 있는 다양한 특징들을 컴파일 단계에서 어떻게 구현되는 가를 학습한다."
+
+					}	
+				},
+				{
+					name:"컴퓨터네트워크",
+					description:{
+						text:"네트워크의 기본 개념과 네트워크의 구조, TCP/IP 프로토콜 구조, 네트워크, 전송 및 응용 계층 프로토콜들에 관련한 기술 및 원리에 대해 인터넷을 중심으로 학습한다."
+
+					}	
+				},
+				{
+					name:"창업기획",
+					description:{
+						text:"...."
+
+					}	
+				},
+				{
+					name:"오픈소스전문프로젝트",
+					description:{
+						text:"소프트웨어 개발이 단순한 코딩 작업만이 아니라, 분석 및 설계 과정을 거쳐 개발되어야 함을 이해하고, 오픈 소스 툴을 사용하여 웹과 앱에서 실행되는 어플리케이션을 개발하는 기술을 학습한다."
+
+					}	
+				},
+				{
+					name:"펌웨어프로그래밍",
+					description:{
+						text:"마이크로컴퓨터 시스템의 하드웨어 구성 요소를 이해하고, 마이크로프로세서 실습 장비를 사용하여 하드웨어를 제어하기 위한 시스템 시작 코드 해석, 인터럽트 및 타이머 제어 프로그래밍, 입출력 디바이스 제어를 위한 펌웨어 프로그래밍 기술을 습득한다."
+					}	
+				},
+				{
+					name:"창업설계",
+					description:{
+						text:"......"
+					}	
+				},
+				{
+					name:"정보보호",
+					description:{
+						text:"정보보호 기초 이론 학습과 네트워크 보안 및 시스템 보안의 기본 개념을 이해하고 안전한 정보 시스템 구축 및 안전한 네트워크 서비스를 제공할 수 있는 방법을 습득한다."
+					}	
+				},
+				{
+					name:"인공지능",
+					description:{
+						text:"인공지능의 성공적인 적용분야 및 목표를 살펴보고, 대표적인 지능적인 문제 해결 기법들, 지식표현 방법 및 추론, 지식기반 시스템, 자연어처리, 컴퓨터 비전, 지능로봇 등에 대해서 학습한다."
+					}	
+				},
+				{
+					name:"기계학습",
+					description:{
+						text:"데이터로부터 패턴을 추천하는 기계학습에 대한 핵심적인 원리를 소개한다. 지도학습, 비지도학습, 강화학습의 기본 개념을 소개하며, 주요 기계학습 알고리즘과 딥러닝 기법을 다룬다."
+					}	
+				},
+				{
+					name:"임베디드시스템",
+					description:{
+						text:"ARM기반 임베디드 시스템의 구조 및 시스템 소프트웨어 동작에 대해 학습하고, 리눅스 포팅, 부트로더, 디바이스 드라이버 작성 등의 실습 프로젝트를 수행한다."
+
+					}	
+				},
+				{
+					name:"영상처리",
+					description:{
+						text:"디지털 영상처리에 대한 기본적인 지식과 함께 관련 분야에의 응용에 대한 기술을 심화한다. 그리고 최근의 영상처리이론과 연구결과를 접함으로써 선진 기술들에 대한 이해의 폭을 넓힌다."
+
+					}	
+				},
+				{
+					name:"창업산학세미나1",
+					description:{
+						text:"산업체와 학교의 교류를 통한 기업가정신, 최신 IT기술 동향 및 산업체의 경영철학과 경영방법을 습득한다."
+
+					}	
+				},
+				{
+					name:"컴퓨터교재연구 및 지도법",
+					description:{
+						text:"컴퓨터 교과지도에 필요한 능력을 배양하고 컴퓨터와 그 응용에 관하여 지식을 습득하게 하며 기초적인 프로그래밍 실무를 익힐 수 있는 교재를 연구한다."
+
+					}	
+				},
+				{
+					name:"정보검색",
+					description:{
+						text:"정보 표현 및 저장 방법, 문헌 질의와 매칭과정, 텍스트 분석, 정보검색 검색효과 측정, 효과성 향상기법 등을 포함한 대량 정보의 조직적인 저장, 관리, 검색, 마이닝 기법에 대한 이론과 방법을 학습한다."
+
+					}	
+				},
+				{
+					name:"창업산학세미나2",
+					description:{
+						text:"산업체와 학교의 교류를 통한 최신 IT기술 동향 및 산업체의 경영철학과 경영방법을 습득한다."
+
+					}	
+				},
+				{
+					name:"컴퓨터논리 및 논술",
+					description:{
+						text:"컴퓨터 교과를 중심으로 학생의 새로운 창조적 아이디어로 표현하고, 아이디어의 타당성과 정당성을 체계적으로 전개하는 능력을 함양하도록 한다. 아울러 컴퓨터 논리에 대한 관련 자료의 탐색을 수행하여 그 결과를 정리하고 발표 및 토론하는 방식으로 중등 교육 현장에서 수요자 중심교육을 실시할 수 있도록 한다. 궁극적으로 컴퓨터 교과 교사로서의 기본 자질인 스스로 문제를 해결할 수 있는 창조적 사고력을 기반으로 지식사회가 필요로 하는 논리적 사고 능력을 습득한다."
+
+					}	
+				},
+				{
+					name:"컴퓨터교육론",
+					description:{
+						text:"컴퓨터에 관한 교육과 컴퓨터를 이용한 교육의 이론과 실제를 학습한다. 컴퓨터 교과의 역사적 배경, 컴퓨터 교과교육의 목표, 중·고등학교 교육과정의 분석 등 중등과정의 정보·컴퓨터 교육의 전반에 관하여 학습한다."
+
+					}	
+				},
+				{
+					name:"자연어 처리",
+					description:{
+						text:"사람이 사용하는 언어를 컴퓨터가 이해하여 지능적으로 처리하기 위한 다양한 알고리즘을 학습한다. 특히 웹 등에서 비정형 데이터로 존재하는 자연언어 텍스트를 처리하기 위해 단어 패턴 추출, 텍스트 정규화, 품사 태깅, 의미 모호성 해소, 부분 파싱, 개체명 인식, 관계 추출, 문서 분류 등에 대해 학습하고 이를 구현 및 활용하는 방법을 익힌다."
+
+					}	
+				},
+				{
+					name:"클라우드 컴퓨팅",
+					description:{
+						text:"본 수업은 클라우드에 대한 상세한 개념과 IaaS, PaaS, SaaS, BPaaS과 같은 다양한 클라우드 서비스 모델에 대해 다룬다. 학생들은 Amazon Elastic Cloud, Microsoft’s Azure, Google App Engine을 활용한 프로젝트를 통해서 실질적인 경험을 얻을 수 있다."
+
+					}	
 				}
 			]
 		}
