0c86f03c229e9112de2ae79bd6071a9cf827f62d
mooneey <moon42586@gmail.com>
2020-09-04 22:31:35 +0900

    the whole course & general complete

---
+++ src/data/general.js
@@ -0,0 +1,187 @@
+export const webcomponentsCollection = {
+	name: "General",
+	areas: [
+		{
+			name: "개신기초교양",
+			skills: [
+				{
+					name: "글쓰기와 발표",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					}
+				},
+
+				{
+					name: "Action English",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					},
+
+					skills: [
+						{
+							name: "영어읽기와 토론",
+							description: {
+								text: "Learn how to create new HTML tags with Custom Elements.",
+								links: [
+									["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+									["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+									["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+									["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+									["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+									["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+									["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+								]
+							}
+						},
+					]
+				},
+			]
+		},
+
+		{
+			name: "일반교양",
+			skills: [
+				{
+					name: "역사와 비판적 사고",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					}
+				},
+
+				{
+					name: "공업법규와 창업",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					}
+				},
+			]
+		},
+
+		{
+			name: "확대교양(추천)",
+			skills: [
+				{
+					name: "개신기초교양",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					}
+				},
+			]
+		},
+
+		{
+			name: "자연이공계기초교양",
+			skills: [
+				{
+					name: "수학I",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					},
+
+					skills: [
+						{
+							name: "수학II",
+							description: {
+								text: "Learn how to create new HTML tags with Custom Elements.",
+								links: [
+									["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+									["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+									["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+									["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+									["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+									["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+									["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+								]
+							}
+						},
+					]
+				},
+				{
+					name: "기초통계학 및 실습",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					},
+				},
+
+				{
+					name: "맛보기 물리학 및 실험",
+					description: {
+						text: "Learn how to create new HTML tags with Custom Elements.",
+						links: [
+							["Google Devs - Custom Elements", "https://developers.google.com/web/fundamentals/web-components/customelements"],
+							["MDN - Using Custom Elements", "https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements"],
+							["CSS Tricks - Creating a Custom Element from Scratch", "https://css-tricks.com/creating-a-custom-element-from-scratch/"],
+							["Javascript.info - Custom Elements", "https://javascript.info/custom-elements"],
+							["dev.to - An introduction to Custom Elements", "https://dev.to/jamesrweb/an-introduction-to-custom-elements-5327"],
+							["itnext - Introduction to Web Components", "https://itnext.io/introduction-to-web-components-part-i-custom-elements-4de6713cef9d"],
+							["web.dev - Web Components", "https://web.dev/web-components-io-2019/"]
+						]
+					},
+				},
+			]
+		},
+	]
+}
